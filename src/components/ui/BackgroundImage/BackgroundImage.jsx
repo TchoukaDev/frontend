ad4272380c0f3112d33dde798f831cefdef6f350
Romain Wirth <romain.wirth@gmail.com>
2025-10-04 15:00:30 +0200

    test

--- src/components/ui/BackgroundImage/BackgroundImage.jsx
+++ src/components/ui/BackgroundImage/BackgroundImage.jsx
@@ -2,12 +2,12 @@ import { fetchStrapi } from "@/utils/fetchStrapi";
 import Image from "next/image";
 
 export default async function BackgroundImage() {
-  const { image } = await fetchStrapi("accueil/optimized");
+  const { image = {} } = await fetchStrapi("accueil/optimized");
 
   return (
     <div className="inset-0 fixed -z-50 ">
       <Image
-        src={`${process.env.STRAPI_API_URL}${image.url}` || "/images/fond.jpg"}
+        src={`${process.env.STRAPI_API_URL}${image?.url}` || "/images/fond.jpg"}
         className="object-cover"
         alt={`${image.alternativeText}` || "Background Image"}
         fill

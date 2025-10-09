461c88c11bb8b454112b4a73dd299d2b9c83a417
Romain Wirth <romain.wirth@gmail.com>
2025-10-09 17:13:36 +0200

    Test fix BaclgroundImage

--- src/components/ui/BackgroundImage/BackgroundImage.jsx
+++ src/components/ui/BackgroundImage/BackgroundImage.jsx
@@ -5,14 +5,13 @@ export default async function BackgroundImage() {
   const { image = {} } = await fetchStrapi("accueil/optimized");
 
   return (
-    <div className="inset-0 fixed -z-50 ">
+    <div className="fixed -z-50 ">
       <Image
         src={`${image?.url}` || "/images/fond.jpg"}
         className="object-cover"
         alt={`${image.alternativeText}` || "Background Image"}
         fill
         priority
-        quality={80}
         sizes="100vw"
       />
     </div>

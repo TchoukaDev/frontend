81edc08040d96e4cfc5649f923bea8a7065cc4eb
Romain Wirth <romain.wirth@gmail.com>
2025-10-11 06:05:00 +0200

    feature: wrapper Suspense ResetPasswordForm

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,5 +1,7 @@
 import ResetPasswordForm from "@/components/Forms/ResetPasswordForm/ResetPasswordForm";
 import Card from "@/components/ui/Card/Card";
+import { Suspense } from "react";
+import { ClipLoader } from "react-spinners";
 
 export default function ResetPassword() {
   return (
@@ -9,7 +11,15 @@ export default function ResetPassword() {
         <p className="text-center prose max-w-none">
           Veuillez saisir votre nouveau mot de passe et le confirmer:
         </p>
-        <ResetPasswordForm />
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center text-blue3">
+              <ClipLoader />
+            </div>
+          }
+        >
+          <ResetPasswordForm />
+        </Suspense>
       </section>
     </Card>
   );

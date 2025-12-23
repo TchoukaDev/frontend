253785391ca80f3d98d515424f28c88d18447ad6
Romain Wirth <romain.wirth@gmail.com>
2025-12-23 17:04:36 +0100

    fix selector max-w Caroussel

--- src/components/ui/Carousel/Carousel.jsx
+++ src/components/ui/Carousel/Carousel.jsx
@@ -170,7 +170,7 @@ const Carousel = ({
           {/* Sélecteur de dossier */}
           <div className="mx-auto relative w-fit">
             <select
-              className="border border-gray-300 rounded-md px-5 py-1 pr-8 text-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer min-w-[70px] max-w-[300px] bg-sand"
+              className="border border-gray-300 rounded-md px-5 py-1 pr-8 text-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer min-w-[70px]  max-w-[300px] md:max-w-[400px] bg-sand"
               value={selectedFolder || "all"}
               onChange={(e) => onChangeFolder(e.target.value)}
             >
